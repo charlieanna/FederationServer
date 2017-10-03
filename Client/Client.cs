31346e583f04031c41b8f84aa9450864700c00e9
Ankur Kothari <ankothari@gmail.com>
2017-10-03 12:54:30 -0400

    BuildRequest added 2 more cases.

--- Client/Client.cs
+++ Client/Client.cs
@@ -39,6 +39,7 @@ namespace FederationServer
             "Creating Build Request".title();
             Console.WriteLine();
 
+            // CSharp Build pass and Tests pass
             Build.TestElement te1 = new Build.TestElement();
             te1.toolchain = "csharp";
             te1.testName = "test1";
@@ -46,6 +47,7 @@ namespace FederationServer
             te1.addCode("Tested1.cs");
             te1.addCode("Tested2.cs");
 
+            // Java Build pass and Tests pass
             Build.TestElement te2 = new Build.TestElement();
             te2.testName = "test2";
             te2.toolchain = "java";
@@ -53,10 +55,28 @@ namespace FederationServer
             te2.addCode("Tested1.java");
             te2.addCode("Tested2.java");
 
+            // CSharp Build pass and Tests Fail
+            Build.TestElement te3 = new Build.TestElement();
+            te3.toolchain = "csharp";
+            te3.testName = "test3";
+            te3.addDriver("TestDriver.cs");
+            te3.addCode("Tested3.cs");
+
+            // CSharp Build Fail
+            Build.TestElement te4 = new Build.TestElement();
+            te4.toolchain = "csharp";
+            te4.testName = "test4";
+            te4.addDriver("TestDriverBuildFailed.cs");
+            te4.addCode("Tested1.cs");
+            te4.addCode("Tested2.cs");
+
+
             BuildRequest tr = new BuildRequest();
             tr.author = "Jim Fawcett";
             tr.tests.Add(te1);
             tr.tests.Add(te2);
+            tr.tests.Add(te3);
+            tr.tests.Add(te4);
             string trXml = tr.ToXml();
             Console.Write("\n  Serialized TestRequest data structure:\n\n  {0}\n", trXml);
             File.WriteAllText(RepoStorage + "/BuildRequest.xml", trXml);
