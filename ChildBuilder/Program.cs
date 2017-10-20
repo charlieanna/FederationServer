901b7014ad30bc9fee07a7eb78db2a194766fbfa
Ankur Kothari <ankothari@gmail.com>
2017-10-20 18:58:43 -0400

    adds SpawnProc

---
+++ ChildBuilder/Program.cs
@@ -0,0 +1,41 @@
+﻿/////////////////////////////////////////////////////////////////////
+// ChildProc - demonstrate creation of multiple .net processes     //
+//                                                                 //
+// Jim Fawcett, CSE681 - Software Modeling and Analysis, Fall 2017 //
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ChildBuilder
+{
+    class ChildBuilder
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = nameof(ChildBuilder);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.Write("\n  Demo Child Process");
+            Console.Write("\n ====================");
+
+            if (args.Count() == 0)
+            {
+                Console.Write("\n  please enter integer value on command line");
+                return;
+            }
+            else
+            {
+                Console.Write("\n  Hello from child #{0}\n\n", args[0]);
+            }
+            Console.Write("\n  Press key to exit");
+            Console.ReadKey();
+            Console.Write("\n  ");
+        }
+    }
+}
--- Executive/Executive.cs
+++ Executive/Executive.cs
@@ -6,7 +6,10 @@
 
 using FederationServer;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+
 namespace Executive
 {
     public class Executive : CommunicatorBase
@@ -46,7 +49,7 @@ namespace Executive
 
     public class TestMsgPass
     {
-        private static void Main()
+        private static void Main1()
         {
             try
             {
@@ -62,6 +65,61 @@ namespace Executive
             {
                 Console.WriteLine("An error occurred: '{0}'", e.Message);
             }
+        }
+
+        static bool createProcess(int i)
+        {
+            using (var proc = new Process())
+            {
+                var fileName = "..\\..\\..\\ChildBuilder\\bin\\debug\\ChildBuilder.exe";
+                string absFileSpec = Path.GetFullPath(fileName);
+
+                Console.Write("\n  attempting to start {0}", absFileSpec);
+                string commandline = i.ToString();
+                try
+                {
+                    Process.Start(fileName, commandline);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("\n  {0}", ex.Message);
+                    return false;
+                }
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "SpawnProc";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.Write("\n  Demo Parent Process");
+            Console.Write("\n =====================");
+
+            if (args.Count() == 0)
+            {
+                Console.Write("\n  please enter number of processes to create on command line");
+                return;
+            }
+            else
+            {
+                int count = Int32.Parse(args[0]);
+                for (int i = 1; i <= count; ++i)
+                {
+                    if (createProcess(i))
+                    {
+                        Console.Write(" - succeeded");
+                    }
+                    else
+                    {
+                        Console.Write(" - failed");
+                    }
+                }
+            } 
+            Console.Write("\n  Press key to exit");
+            Console.ReadKey();
+            Console.Write("\n  ");
         }
     }
 }
